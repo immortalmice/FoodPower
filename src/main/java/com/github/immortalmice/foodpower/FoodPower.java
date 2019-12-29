23e553cac99dcc0d81f2e3dd5f122d8880013621
immortalmice <immortalmice@users.noreply.github.com>
2019-12-29 12:30:52 +0800

    First Build
    The first build
    Finish proxy

---
+++ src/main/java/com/github/immortalmice/foodpower/FoodPower.java
@@ -0,0 +1,43 @@
+package com.github.immortalmice.foodpower;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.Mod.Instance;
+
+import net.minecraftforge.fml.common.SidedProxy;
+
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+import com.github.immortalmice.foodpower.common.CommonProxy;
+
+@Mod(modid = FoodPower.MODID, name = FoodPower.NAME, version = FoodPower.VERSION)
+public class FoodPower{
+    public static final String MODID = "foodpower";
+    public static final String NAME = "Food Power";
+    public static final String VERSION = "0.1";
+
+    @Instance(FoodPower.MODID)
+    public static FoodPower instance;
+
+    @SidedProxy(clientSide = "com.github.immortalmice.foodpower.client.ClientProxy",
+        serverSide = "com.github.immortalmice.foodpower.common.CommonProxy")
+    public static CommonProxy proxy;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event){
+        proxy.preInit(event);
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event){
+        proxy.init(event);
+    }
+
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event){
+        proxy.postInit(event);
+    }
+
+}
---
+++ src/main/java/com/github/immortalmice/foodpower/client/ClientProxy.java
@@ -0,0 +1,24 @@
+package com.github.immortalmice.foodpower.client;
+
+import com.github.immortalmice.foodpower.common.CommonProxy;
+
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+
+public class ClientProxy extends CommonProxy{
+	@Override
+	public void preInit(FMLPreInitializationEvent event){
+		super.preInit(event);
+	}
+
+	@Override
+	public void init(FMLInitializationEvent event){
+		super.init(event);
+	}
+
+	@Override
+	public void postInit(FMLPostInitializationEvent event){
+		super.postInit(event);
+	}
+}
