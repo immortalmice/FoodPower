d5bae7a601236101dd72c944b1dee82394c0262e
YenHsinWu <kelly850408@gmail.com>
2020-01-08 11:04:54 +0800

    Add butter texture

--- src/main/java/com/github/immortalmice/foodpower/registry/RegistryHandler.java
+++ src/main/java/com/github/immortalmice/foodpower/registry/RegistryHandler.java
@@ -1,9 +1,11 @@
 package com.github.immortalmice.foodpower.registry;
 
 import net.minecraft.item.Item;
+import net.minecraft.block.Block;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 
@@ -16,12 +18,12 @@ public class RegistryHandler{
     	event.getRegistry().registerAll(IngredientList.list.toArray(new Item[0]));
     }
     @SubscribeEvent
-    public static void onItemRegister(RegistryEvent.Register<Block> event){
+    public static void onBlockRegister(RegistryEvent.Register<Block> event){
 
     }
     @SubscribeEvent
     public static void onModelReg(ModelRegistryEvent event) {
-    	for(int i = 0; i <= IngredientList.list.size(); i ++){
+    	for(int i = 0; i <= IngredientList.list.size()-1; i ++){
     		Item item = IngredientList.list.get(i);
     		ModelResourceLocation marl = new ModelResourceLocation(item.getRegistryName(), "inventory");
         	ModelLoader.setCustomModelResourceLocation(item, 0, marl);
