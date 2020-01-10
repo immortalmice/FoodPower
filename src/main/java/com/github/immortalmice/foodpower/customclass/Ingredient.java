81b435f1196c8849f8b045aa416a3bc0a05fc295
immortalmice <immortalmice@users.noreply.github.com>
2020-01-10 12:41:55 +0800

    Three Second Rule
    Add Three Second Rule!

--- src/main/java/com/github/immortalmice/foodpower/customclass/Ingredient.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/Ingredient.java
@@ -1,6 +1,9 @@
 package com.github.immortalmice.foodpower.customclass;
 
 import net.minecraft.item.ItemFood;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.nbt.NBTTagCompound;
 
 import com.github.immortalmice.foodpower.lists.FPCreativeTabs;
 import com.github.immortalmice.foodpower.lists.Ingredients;
@@ -17,4 +20,17 @@ public class Ingredient extends ItemFood{
         /** Add to ingredient list, and regist it later */
         Ingredients.list.add(this);
 	}
+
+	/** Three Second Rule! */
+	@Override
+	public boolean onEntityItemUpdate(EntityItem entityItem){
+		if(this.getRegistryName().toString() != "dirty_food"){
+			NBTTagCompound compound = new NBTTagCompound();
+			entityItem.writeEntityToNBT(compound);
+			if(compound.getShort("Age") >= 20 * 3){
+				entityItem.setItem(new ItemStack(Ingredients.DIRTY_FOOD, entityItem.getItem().getCount()));
+			}
+		}
+		return false;
+	}		
 }
