09bdec545e1e5077b62fa60c9c794976a2e90833
immortalmice <immortalmice@users.noreply.github.com>
2020-01-26 05:52:34 +0800

    NormalEventHandler
    NormalEventHandler

---
+++ src/main/java/com/github/immortalmice/foodpower/event/NormalEventHandler.java
@@ -0,0 +1,10 @@
+package com.github.immortalmice.foodpower.event;
+
+import net.minecraftforge.fml.common.Mod;
+
+import com.github.immortalmice.foodpower.FoodPower;
+
+@Mod.EventBusSubscriber(modid = FoodPower.MODID)
+public class NormalEventHandler{
+
+}
