176c8be36da1a61f086be1178a6a0597ff5a6d4c
immortalmice <immortalmice@users.noreply.github.com>
2020-02-13 02:17:46 +0800

    bounding block
    bounding block in kitchenAppliance

--- src/main/java/com/github/immortalmice/foodpower/customclass/KitchenAppliance.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/KitchenAppliance.java
@@ -1,8 +1,12 @@
 package com.github.immortalmice.foodpower.customclass;
 
 import net.minecraft.util.BlockRenderLayer;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -10,9 +14,12 @@ import com.github.immortalmice.foodpower.baseclass.BlockRotatableBase;
 import com.github.immortalmice.foodpower.lists.KitchenAppliances;
 
 public class KitchenAppliance extends BlockRotatableBase{
-	public KitchenAppliance(String name){
+	AxisAlignedBB blockAABB;
+
+	public KitchenAppliance(String name, AxisAlignedBB blockAABBIn){
 		super(name, Material.IRON);
 
+		this.blockAABB = blockAABBIn;
 		/** How dare you use stone pickaxe break this holy things */
 		this.setHarvestLevel("pickaxe", 2);
 		this.setHardness(1.5f);
@@ -22,9 +29,15 @@ public class KitchenAppliance extends BlockRotatableBase{
         KitchenAppliances.list.add(this);
 	}
 
+	/* Make it transparent in some part */
 	@SideOnly(Side.CLIENT)
 	@Override
     public BlockRenderLayer getRenderLayer(){
         return BlockRenderLayer.CUTOUT_MIPPED;
+    }
+
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos){
+    	return this.blockAABB;
     }
 }
