1c46e55e9c3978bee22bca48699213af6011dea3
immortalmice <immortalmice@users.noreply.github.com>
2020-02-25 03:21:30 +0800

    First Commit
    First Commit

---
+++ src/main/java/com/github/immortalmice/foodpower/FoodPower.java
@@ -0,0 +1,10 @@
+package com.github.immortalmice.foodpower;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(FoodPower.MODID)
+public class FoodPower{
+    public static final String MODID = "foodpower";
+    public static final String NAME = "Food Power";
+    public static final String VERSION = "0.1";
+}
