942b5a6f142d4061b541da92e67c5c2d59aa0bf4
immortalmice <immortalmice@users.noreply.github.com>
2020-02-26 02:01:15 +0800

    Fix Errors (Unfinished)
    Fix Errors (Unfinished)

--- src/main/java/com/github/immortalmice/foodpower/customclass/KitchenAppliance.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/KitchenAppliance.java
@@ -5,8 +5,10 @@ import net.minecraft.block.SoundType;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.material.Material;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.util.math.shapes.ISelectionContext;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.common.ToolType;
 
@@ -16,14 +18,14 @@ import com.github.immortalmice.foodpower.lists.KitchenAppliances;
 public class KitchenAppliance extends BlockBase{
 	VoxelShape blockAABB;
 
-	public KitchenAppliance(String name, VoxelShape blockAABBIn){
+	public KitchenAppliance(String name, AxisAlignedBB blockAABBIn){
 		super(name, Block.Properties.create(Material.IRON)
 			.harvestLevel(2)
 			.harvestTool(ToolType.PICKAXE)
 			.sound(SoundType.METAL)
 			.hardnessAndResistance(1.5f));
 
-		this.blockAABB = blockAABBIn;
+		this.blockAABB = VoxelShapes.create(blockAABBIn);
 
         /** Add to kitchen appliances list, and regist it later */
         KitchenAppliances.list.add(this);
--- src/main/java/com/github/immortalmice/foodpower/customclass/crop/CropBlock.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/crop/CropBlock.java
@@ -2,36 +2,47 @@ package com.github.immortalmice.foodpower.customclass.crop;
 
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.block.BlockCrops;
-import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.Item;
-import net.minecraft.init.Blocks;
+import net.minecraft.block.Blocks;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.CropsBlock;
+import net.minecraft.block.material.Material;
 import net.minecraft.world.World;
+import net.minecraft.world.IBlockReader;
 
 import com.github.immortalmice.foodpower.FoodPower;
 import com.github.immortalmice.foodpower.baseclass.ItemFoodBase;
 import com.github.immortalmice.foodpower.lists.Crops;
 
-public class CropBlock extends BlockCrops{
+public class CropBlock extends CropsBlock{
 	private String name;
 	public CropBlock(String nameIn, ItemFoodBase cropIn){
-		super();
+		super(Properties.create(Material.PLANTS)
+			.hardnessAndResistance(0.0F)
+			.doesNotBlockMovement()
+			.sound(SoundType.PLANT));
 
-		this.setTranslationKey(nameIn.concat("_crop"));
+        this.name = nameIn;
+
         this.setRegistryName(nameIn.concat("_crop"));
         
-        name = nameIn;
-
         Crops.blockList.add(this);
 	}
 
-	protected boolean canSustainBush(IBlockState state){
+	@Override
+	public String getTranslationKey(){
+		return this.name.concat("_crop");
+	}
+
+	@Override
+	protected boolean isValidGround(BlockState state, IBlockReader worldIn, BlockPos pos){
 		return state.getBlock() == Blocks.FARMLAND;
 	}
 
-	public boolean canBlockStay(World worldIn, BlockPos pos, IBlockState state){
-		IBlockState soil = worldIn.getBlockState(pos.down());
-		return (worldIn.getLight(pos) >= 8 || worldIn.canSeeSky(pos)) && soil.getBlock() == Blocks.FARMLAND;
+	public boolean canBlockStay(World worldIn, BlockPos pos, BlockState state){
+		BlockState soil = worldIn.getBlockState(pos.down());
+		return (worldIn.getLight(pos) >= 8 || worldIn.func_226660_f_(pos)) && soil.getBlock() == Blocks.FARMLAND;
 	}
 
 	protected Item getSeed(){
