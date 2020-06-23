6ba6ba5bd77230b1afdd8dac3dc5375b7a860216
immortalmice <killer536697@gmail.com>
2020-06-23 22:55:30 +0800

    Fix: PapayaSeed Entity dose not shown in client
    Cause:
        The Renderer of this entity did not been registed.
        Need to override createSpawn Packet to handle the packet.
    Solution:
        Regist the renderer of PapayaSeedEntity.
        Override createSpawnPacket to return a packet provide from Forge.
    Add:
        Imports that is needed.

--- src/main/java/com/github/immortalmice/foodpower/customclass/specialclass/PapayaSeedEntity.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/specialclass/PapayaSeedEntity.java
@@ -1,12 +1,15 @@
 package com.github.immortalmice.foodpower.customclass.specialclass;
 
+import com.github.immortalmice.foodpower.lists.OtherEntitys;
 import com.github.immortalmice.foodpower.lists.OtherItems;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.projectile.ProjectileItemEntity;
 import net.minecraft.item.Item;
+import net.minecraft.network.IPacket;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
 
 public class PapayaSeedEntity extends ProjectileItemEntity{
 	public PapayaSeedEntity(EntityType<? extends PapayaSeedEntity> type, World worldIn){
@@ -16,6 +19,11 @@ public class PapayaSeedEntity extends ProjectileItemEntity{
 	@Override
 	protected Item getDefaultItem() {
 		return OtherItems.Items.PAPAYA_SEED;
+	}
+
+	@Override
+	public IPacket<?> createSpawnPacket(){
+		return NetworkHooks.getEntitySpawningPacket(this);
 	}
 
 	@Override
--- src/main/java/com/github/immortalmice/foodpower/handlers/RenderHandler.java
+++ src/main/java/com/github/immortalmice/foodpower/handlers/RenderHandler.java
@@ -1,18 +1,27 @@
 package com.github.immortalmice.foodpower.handlers;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.client.renderer.entity.EntityRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
+import net.minecraft.entity.Entity;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 
 import com.github.immortalmice.foodpower.customclass.KitchenAppliance;
 import com.github.immortalmice.foodpower.customclass.crop.CropBlock;
 import com.github.immortalmice.foodpower.customclass.tree.TreeSaplingBush;
 import com.github.immortalmice.foodpower.lists.Crops;
 import com.github.immortalmice.foodpower.lists.KitchenAppliances;
+import com.github.immortalmice.foodpower.lists.OtherEntitys;
 import com.github.immortalmice.foodpower.lists.Trees;
 
 public class RenderHandler{
 	public static void setupClient(){
 		RenderHandler.setupBlockRenderLayer();
+		RenderHandler.registEntityRenderer();
 	}
 
 	private static void setupBlockRenderLayer(){
@@ -25,5 +34,18 @@ public class RenderHandler{
 		for(TreeSaplingBush block : Trees.saplingBushList){
 			RenderTypeLookup.setRenderLayer(block, RenderType.getCutoutMipped());
 		}
+	}
+
+	private static void registEntityRenderer(){
+		RenderingRegistry.registerEntityRenderingHandler(OtherEntitys.EntityTypes.PAPAYA_SEED, RenderHandler.getSpriteIRenderFactory());
+	}
+
+	private static IRenderFactory<Entity> getSpriteIRenderFactory(){
+		return new IRenderFactory<Entity>(){
+			@SuppressWarnings({ "unchecked", "rawtypes" })
+			public EntityRenderer<? super Entity> createRenderFor(EntityRendererManager manager){
+				return new SpriteRenderer(manager, Minecraft.getInstance().getItemRenderer());
+			}
+		};
 	}
 }
