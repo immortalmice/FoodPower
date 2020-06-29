3e87e4361d924c40e72023704489494d5d50204d
immortalmice <killer536697@gmail.com>
2020-06-29 22:49:59 +0800

    Refactor: Change the field name from "fpName" to "name"
    Reason:
        The old design is outdated, just using "name" will be better.
    Modify:
        The field name from "fpName" to "name".
        The method name from "getFPName" to "getName".
        Use field directly in constructor instead of using method.
    
    BREAKING CHANGE:
        Before:
            public String getFPName()
        After:
            public String getName()

--- src/main/java/com/github/immortalmice/foodpower/customclass/container/ContainerPack.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/container/ContainerPack.java
@@ -7,19 +7,19 @@ import com.github.immortalmice.foodpower.baseclass.ContainerBase;
 import com.github.immortalmice.foodpower.lists.Containers;
 
 public class ContainerPack<T extends ContainerBase>{
-	private final String fpName;
+	private final String name;
 	private final IContainerFactory<T> factory;
 
 	public ContainerPack(String nameIn, IContainerFactory<T> factoryIn){
 		this.factory = factoryIn;
-		this.fpName = nameIn;
+		this.name = nameIn;
 
 		Containers.list.add(this);
-		Containers.REGISTER.register(this.getFPName(), () -> this.getContainerType());
+		Containers.REGISTER.register(this.name, () -> this.getContainerType());
 	}
 
-	public String getFPName(){
-		return this.fpName;
+	public String getName(){
+		return this.name;
 	}
 
 	public ContainerType<T> getContainerType(){
