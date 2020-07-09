f1f801210743db680d1902ad3784389fe59b4d5f
immortalmice <killer536697@gmail.com>
2020-07-09 23:50:39 +0800

    Refactor: Remove unnecessary part in TileEntityPack
    Reason:
        They "seems" to be unnecessary.
    
    BREAKING CHANGE:
        Before:
            public String getFPName()
        After:
            <Remove>
    Note:
        Will add them back if they are needed in the future.

--- src/main/java/com/github/immortalmice/foodpower/customclass/tileentity/TileEntityPack.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/tileentity/TileEntityPack.java
@@ -4,28 +4,21 @@ import java.util.function.Supplier;
 
 import net.minecraft.block.Block;
 import net.minecraft.tileentity.TileEntityType;
-import net.minecraftforge.registries.ForgeRegistryEntry;
 
 import com.github.immortalmice.foodpower.baseclass.TileEntityBase;
 import com.github.immortalmice.foodpower.lists.TileEntitys;
 
-public class TileEntityPack<T extends TileEntityBase> extends ForgeRegistryEntry<TileEntityType<?>>{
+public class TileEntityPack<T extends TileEntityBase>{
 	private final TileEntityType<T> tileEntityType;
-	private final String fpName;
 
 	public TileEntityPack(String nameIn, Supplier<T> factoryIn, Block... blocksIn){
 		this.tileEntityType = TileEntityType.Builder.create(factoryIn, blocksIn).build(null);
-		this.fpName = nameIn;
 
 		TileEntitys.list.add(this);
-		TileEntitys.REGISTER.register(this.getFPName(), () -> this.getTileEntityType());
+		TileEntitys.REGISTER.register(nameIn, () -> this.getTileEntityType());
 	}
 
 	public TileEntityType<T> getTileEntityType(){
 		return this.tileEntityType;
-	}
-
-	public String getFPName(){
-		return this.fpName;
 	}
 }
