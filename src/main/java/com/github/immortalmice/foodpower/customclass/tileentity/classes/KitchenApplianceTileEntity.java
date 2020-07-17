732bfa836a52bbd36341e08f7e7db49f6ade2826
immortalmice <killer536697@gmail.com>
2020-07-18 00:22:51 +0800

    Fix: Read & Write NBT did not work correctly
    Cause:
        According to TileEntity's creation, the world field may be null.
    Solution:
        Write block's RegistryName, and get block by the name instead of BlockPos.
    Add:
        Override method write to add controls on NBT writing.
        Nullable annotation on getBlock method.
        Const string that defined the key used in storing block to NBT.
    Change:
        Default value of block when using non-parameter constructor
            , in this case, TileEntity will get block by reading NBT.
        Implement of read method.

--- src/main/java/com/github/immortalmice/foodpower/customclass/tileentity/classes/KitchenApplianceTileEntity.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/tileentity/classes/KitchenApplianceTileEntity.java
@@ -1,23 +1,30 @@
 package com.github.immortalmice.foodpower.customclass.tileentity.classes;
 
+import javax.annotation.Nullable;
+
 import com.github.immortalmice.foodpower.baseclass.TileEntityBase;
 import com.github.immortalmice.foodpower.customclass.KitchenAppliance;
-import com.github.immortalmice.foodpower.lists.KitchenAppliances;
 import com.github.immortalmice.foodpower.lists.TileEntitys;
 
 import net.minecraft.block.Block;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.registries.ForgeRegistries;
 
 public class KitchenApplianceTileEntity extends TileEntityBase{
 	private KitchenAppliance block;
+	private static final String BLOCK_NBT_KEY = "kitchen_appliance_block";
 
 	public KitchenApplianceTileEntity(){
-		this(KitchenAppliances.Blocks.OVEN);
+		this(null);
 	}
 	public KitchenApplianceTileEntity(KitchenAppliance blockIn){
 		super(TileEntitys.TileEntityTypes.KITCHEN_APPLIANCE);
+		
+		this.block = blockIn;
 	}
 
+	@Nullable
 	public KitchenAppliance getBlock(){
 		return this.block;
 	}
@@ -25,9 +32,21 @@ public class KitchenApplianceTileEntity extends TileEntityBase{
 	@Override
 	public void read(CompoundNBT nbt){
 		super.read(nbt);
-		Block block = this.world.getBlockState(this.pos).getBlock();
-		if(block instanceof KitchenAppliance){
-			this.block = (KitchenAppliance) block;
+		if(nbt.contains(KitchenApplianceTileEntity.BLOCK_NBT_KEY)){
+			Block nbtBlock = ForgeRegistries.BLOCKS.getValue(
+				new ResourceLocation(nbt.getString(KitchenApplianceTileEntity.BLOCK_NBT_KEY)));
+			
+			if(nbtBlock != null && nbtBlock instanceof KitchenAppliance){
+				this.block = (KitchenAppliance) block;
+			}
 		}
+	}
+
+	@Override
+	public CompoundNBT write(CompoundNBT nbt){
+		if(this.block != null){
+			nbt.putString(KitchenApplianceTileEntity.BLOCK_NBT_KEY, this.block.getRegistryName().toString());
+		}
+		return super.write(nbt);
 	}
 }
