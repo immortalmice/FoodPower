d6e3313e8b505064ada62ea289148cf823835791
immortalmice <killer536697@gmail.com>
2020-08-13 22:24:49 +0800

    Feat: Add ItemHandler capability to KitchenApplianceTileEntity
    Add:
        Class KitchenApplianceItemHandler to handle special logic of this capability.
        Codes to declare and provide ItemHandler capability.
        Codes to store item stack info into nbt.
        Slots in KitchenApplianceContainer.

--- src/main/java/com/github/immortalmice/foodpower/customclass/container/classes/kitchenappliance/KitchenApplianceContainer.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/container/classes/kitchenappliance/KitchenApplianceContainer.java
@@ -5,25 +5,33 @@ import javax.annotation.Nullable;
 import com.github.immortalmice.foodpower.baseclass.ContainerBase;
 import com.github.immortalmice.foodpower.customclass.KitchenAppliance;
 import com.github.immortalmice.foodpower.customclass.tileentity.classes.KitchenApplianceTileEntity;
+import com.github.immortalmice.foodpower.customclass.tileentity.classes.KitchenApplianceTileEntity.KitchenApplanceItemHandler;
 import com.github.immortalmice.foodpower.lists.Containers;
 
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraftforge.items.SlotItemHandler;
 
 public class KitchenApplianceContainer extends ContainerBase{
 	private KitchenApplianceTileEntity tileEntity;
+	private KitchenApplanceItemHandler itemHandler;
 
 	public KitchenApplianceContainer(int windowId, PlayerInventory playerInventory, PacketBuffer extraData){
 		this(windowId, playerInventory, extraData.readBlockPos());
 	}
+
 	public KitchenApplianceContainer(int windowId, PlayerInventory playerInventory, BlockPos posIn){
 		super(Containers.ContainerTypes.KITCHEN_APPLIANCE, windowId, new int[]{45, 145}, playerInventory);
 
 		TileEntity tile = playerInventory.player.world.getTileEntity(posIn);
 		if(tile instanceof KitchenApplianceTileEntity){
 			this.tileEntity = (KitchenApplianceTileEntity) tile;
+			this.itemHandler = this.tileEntity.getItemHandler();
+			
+			this.addSlot(new SlotItemHandler(this.itemHandler, 0, 45, 119));
+			this.addSlot(new SlotItemHandler(this.itemHandler, 1, 175, 64));
 		}
 	}
 
--- src/main/java/com/github/immortalmice/foodpower/customclass/tileentity/classes/KitchenApplianceTileEntity.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/tileentity/classes/KitchenApplianceTileEntity.java
@@ -5,10 +5,13 @@ import javax.annotation.Nullable;
 
 import com.github.immortalmice.foodpower.baseclass.TileEntityBase;
 import com.github.immortalmice.foodpower.customclass.KitchenAppliance;
+import com.github.immortalmice.foodpower.customclass.food.CookedFood;
+import com.github.immortalmice.foodpower.customclass.specialclass.RecipeScroll;
 import com.github.immortalmice.foodpower.lists.TileEntitys;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.tileentity.ITickableTileEntity;
 import net.minecraft.util.Direction;
@@ -18,15 +21,21 @@ import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.EnergyStorage;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.IItemHandlerModifiable;
+import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class KitchenApplianceTileEntity extends TileEntityBase implements ITickableTileEntity{
 	private KitchenAppliance block;
+	private KitchenApplanceItemHandler itemHandler = new KitchenApplanceItemHandler();
 	private EnergyStorage energyStorage = new EnergyStorage(1000000);
 	private int catchedEnergyStored = 0;
 
 	private static final String ENERGY_NBT_KEY = "forge_energy";
 	private static final String BLOCK_NBT_KEY = "kitchen_appliance_block";
+	private static final String ITEM_NBT_KEY = "item_handler";
 
 	public KitchenApplianceTileEntity(){
 		this(null);
@@ -57,6 +66,10 @@ public class KitchenApplianceTileEntity extends TileEntityBase implements ITicka
 		if(nbt.contains(KitchenApplianceTileEntity.ENERGY_NBT_KEY)){
 			CapabilityEnergy.ENERGY.readNBT(this.energyStorage, null, nbt.get(KitchenApplianceTileEntity.ENERGY_NBT_KEY));
 		}
+
+		if(nbt.contains(KitchenApplianceTileEntity.ITEM_NBT_KEY)){
+			CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.readNBT(this.itemHandler, null, nbt.get(KitchenApplianceTileEntity.ITEM_NBT_KEY));
+		}
 		
 		if(nbt.contains(KitchenApplianceTileEntity.BLOCK_NBT_KEY)){
 			Block nbtBlock = ForgeRegistries.BLOCKS.getValue(
@@ -71,6 +84,7 @@ public class KitchenApplianceTileEntity extends TileEntityBase implements ITicka
 	@Override
 	public CompoundNBT write(CompoundNBT nbt){
 		nbt.put(KitchenApplianceTileEntity.ENERGY_NBT_KEY, CapabilityEnergy.ENERGY.writeNBT(this.energyStorage, null));
+		nbt.put(KitchenApplianceTileEntity.ITEM_NBT_KEY, CapabilityItemHandler.ITEM_HANDLER_CAPABILITY.writeNBT(this.itemHandler, null));
 		if(this.block != null){
 			nbt.putString(KitchenApplianceTileEntity.BLOCK_NBT_KEY, this.block.getRegistryName().toString());
 		}
@@ -82,11 +96,14 @@ public class KitchenApplianceTileEntity extends TileEntityBase implements ITicka
 		if(cap == CapabilityEnergy.ENERGY && this.block != null && this.block.isElectrical()){
 			return LazyOptional.of(() -> this.energyStorage).cast();
 		}
+		if(cap == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY && this.block != null){
+			return LazyOptional.of(() -> this.itemHandler).cast();
+		}
 		return super.getCapability(cap, side);
 	}
 
 	@Override
-	public void tick() {
+	public void tick(){
 		if(this.hasWorld() && !this.world.isRemote){
 			BlockState blockState = this.getBlockState();
 
@@ -96,6 +113,128 @@ public class KitchenApplianceTileEntity extends TileEntityBase implements ITicka
 				this.markDirty();
 
 				this.catchedEnergyStored = this.energyStorage.getEnergyStored();
+			}
+		}
+	}
+
+	public KitchenApplanceItemHandler getItemHandler(){
+		return this.itemHandler;
+	}
+
+	public class KitchenApplanceItemHandler implements IItemHandler, IItemHandlerModifiable{
+		private ItemStackHandler scrollHandler = new ItemStackHandler();
+		private ItemStackHandler outputHandler = new ItemStackHandler();
+		private ItemStackHandler ingredientHandler = new ItemStackHandler();
+
+		private KitchenApplanceItemHandler(){
+
+		}
+
+		private ItemStack getScroll(){
+			ItemStack stack = this.scrollHandler.getStackInSlot(0);
+			if(!stack.isEmpty() && stack.getItem() instanceof RecipeScroll){
+				return stack;
+			}
+			return ItemStack.EMPTY;
+		}
+
+		@Override
+		public int getSlots(){
+			// Cause scroll & output slot always exist. 
+			return 2 + ingredientHandler.getSlots();
+		}
+
+		@Override
+		public ItemStack getStackInSlot(int slot) {
+			if(slot >= 0 && slot <= this.getSlots()-1){
+				switch(slot){
+					case 0:
+						return this.scrollHandler.getStackInSlot(0);
+					case 1:
+						return this.outputHandler.getStackInSlot(0);
+					default:
+						return this.ingredientHandler.getStackInSlot(slot - 2);
+				}
+			}
+			return ItemStack.EMPTY;
+		}
+
+		@Override
+		public ItemStack insertItem(int slot, ItemStack stack, boolean simulate){
+			KitchenApplianceTileEntity.this.markDirty();
+			return ItemStack.EMPTY;
+		}
+
+		@Override
+		public ItemStack extractItem(int slot, int amount, boolean simulate){
+			ItemStack stack = this.getStackInSlot(slot);
+			if(!stack.isEmpty()){
+				ItemStack extractStack = stack.copy();
+
+				int extractCount = Math.min(stack.getCount(), amount);
+				extractStack.setCount(extractCount);
+
+				if(!simulate){
+					stack.setCount(stack.getCount() - extractCount);
+					KitchenApplianceTileEntity.this.markDirty();
+				}
+				return extractStack;
+			}
+			return ItemStack.EMPTY;
+		}
+
+		@Override
+		public int getSlotLimit(int slot) {
+			if(slot < 0 || slot > this.getSlots()-1){
+				return 0;
+			}else{
+				switch(slot){
+					case 0:
+						return 1;
+					case 1:
+						return 64;
+					default:
+						return 1024;
+				}
+			}
+		}
+
+		@Override
+		public boolean isItemValid(int slot, ItemStack stack) {
+			if(!stack.isEmpty()){
+				if(slot < 0 || slot > this.getSlots()-1){
+					return false;
+				}else{
+					switch(slot){
+						case 0:
+							return stack.getItem() instanceof RecipeScroll;
+						case 1:
+							return stack.getItem() instanceof CookedFood;
+						default:
+							if(RecipeScroll.getRequiredItemStacks(this.getScroll()).get(slot - 2).getItem() == stack.getItem()){
+								return true;
+							}
+							return false;
+					}
+				}
+			}
+			return false;
+		}
+
+		@Override
+		public void setStackInSlot(int slot, ItemStack stack) {
+			if(slot >= 0 && slot <= this.getSlots()-1){
+				switch(slot){
+					case 0:
+						this.scrollHandler.setStackInSlot(0, stack);
+						break;
+					case 1:
+						this.outputHandler.setStackInSlot(0, stack);
+						break;
+					default:
+						this.ingredientHandler.setStackInSlot(slot - 2, stack);
+				}
+				KitchenApplianceTileEntity.this.markDirty();
 			}
 		}
 	}
