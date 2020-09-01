a89fcb62dd14fd6695eb6ac8ee53ecbd67398472
immortalmice <killer536697@gmail.com>
2020-09-01 22:24:54 +0800

    Refactor: Extract nbt keys to a static field of interface

--- src/main/java/com/github/immortalmice/foodpower/customclass/cooking/CookingRecipe.java
+++ src/main/java/com/github/immortalmice/foodpower/customclass/cooking/CookingRecipe.java
@@ -31,6 +31,18 @@ public class CookingRecipe{
 
 	private static final float[] RARITY_DISCOUNT = {1.0f, 0.8f, 0.6f, 0.4f};
 
+	public static interface NBT_KEY{
+		String PATTERN = "pattern";
+		String RARITY = "rarity";
+		String RAND = "rand";
+		String OUTPUT_AMOUNT = "output_amount";
+		String DISPLAY_NAME = "display_name";
+		String ID = "id";
+		String INGREDIENTS = "ingredients";
+		String STACK = "stack";
+		String LEVEL = "level";
+	}
+
 	public CookingRecipe(CookingPattern patternIn, List<ItemStack> listIn, String nameIn){
 		this.pattern = patternIn;
 		this.recipeName = nameIn;
@@ -96,12 +108,12 @@ public class CookingRecipe{
 	public static CompoundNBT write(CookingRecipe recipe){
 		CompoundNBT nbt = new CompoundNBT();
 
-		nbt.putString("pattern", recipe.pattern.getName());
-		nbt.putInt("rarity", recipe.rarity);
-		nbt.putFloat("rand", recipe.rand);
-		nbt.putInt("output_amount", recipe.outputAmount);
-		nbt.putString("displayName", recipe.recipeName);
-		nbt.putString("id", recipe.ID);
+		nbt.putString(NBT_KEY.PATTERN, recipe.pattern.getName());
+		nbt.putInt(NBT_KEY.RARITY, recipe.rarity);
+		nbt.putFloat(NBT_KEY.RAND, recipe.rand);
+		nbt.putInt(NBT_KEY.OUTPUT_AMOUNT, recipe.outputAmount);
+		nbt.putString(NBT_KEY.DISPLAY_NAME, recipe.recipeName);
+		nbt.putString(NBT_KEY.ID, recipe.ID);
 
 		ListNBT listNBT = new ListNBT();
 		recipe.ingredients.forEach((pair) -> {
@@ -110,39 +122,40 @@ public class CookingRecipe{
 			ItemStack stack = pair.getFirst();
 			int level = pair.getSecond();
 
-			element.put("stack", stack.write(new CompoundNBT()));
-			element.putInt("level", level);
+			element.put(NBT_KEY.STACK, stack.write(new CompoundNBT()));
+			element.putInt(NBT_KEY.LEVEL, level);
 
 			listNBT.add(element);
 		});
-		nbt.put("ingredients", listNBT);
+		nbt.put(NBT_KEY.INGREDIENTS, listNBT);
 
 		return nbt;
 	}
 
 	@Nullable
 	public static CookingRecipe read(CompoundNBT nbt){
-		if(nbt.contains("pattern") && nbt.contains("ingredients") && nbt.contains("id")){
-			CookingPattern pattern = CookingPatterns.getPatternByName(nbt.getString("pattern"));
+		if(nbt.contains(NBT_KEY.PATTERN) && nbt.contains(NBT_KEY.INGREDIENTS) && nbt.contains(NBT_KEY.ID)){
+			CookingPattern pattern = CookingPatterns.getPatternByName(nbt.getString(NBT_KEY.PATTERN));
 			if(pattern == null) return null;
-			final CookingRecipe recipe = new CookingRecipe(pattern, nbt.getString("id"));
 
-			ListNBT listNBT = (ListNBT) nbt.get("ingredients");
+			final CookingRecipe recipe = new CookingRecipe(pattern, nbt.getString(NBT_KEY.ID));
+
+			ListNBT listNBT = (ListNBT) nbt.get(NBT_KEY.INGREDIENTS);
 			listNBT.forEach((ele) -> {
 				CompoundNBT element = (CompoundNBT) ele;
-				if(element.contains("stack") && element.contains("level")){
-					ItemStack stack = ItemStack.read((CompoundNBT) element.get("stack"));
-					int level = element.getInt("level");
+				if(element.contains(NBT_KEY.STACK) && element.contains(NBT_KEY.LEVEL)){
+					ItemStack stack = ItemStack.read((CompoundNBT) element.get(NBT_KEY.STACK));
+					int level = element.getInt(NBT_KEY.LEVEL);
 
 					recipe.ingredients.add(new Pair<>(stack, level));
 				}
 			});
 
-			recipe.outputAmount = nbt.contains("outputAmount") ? nbt.getInt("outputAmount") : 1;
-			recipe.recipeName = nbt.contains("displayName") ? nbt.getString("displayName") : "";
+			recipe.outputAmount = nbt.contains(NBT_KEY.OUTPUT_AMOUNT) ? nbt.getInt(NBT_KEY.OUTPUT_AMOUNT) : 1;
+			recipe.recipeName = nbt.contains(NBT_KEY.DISPLAY_NAME) ? nbt.getString(NBT_KEY.DISPLAY_NAME) : "";
 
-			recipe.initialize(nbt.contains("rarity") ? nbt.getInt("rarity") : 0
-				, nbt.contains("rand") ? nbt.getFloat("rand") : 1.1f);
+			recipe.initialize(nbt.contains(NBT_KEY.RARITY) ? nbt.getInt(NBT_KEY.RARITY) : 0
+				, nbt.contains(NBT_KEY.RAND) ? nbt.getFloat(NBT_KEY.RAND) : 1.1f);
 
 			return recipe;
 		}
