0e6f88a1224437b0ffc0784ce83517e43970932e
immortalmice <killer536697@gmail.com>
2020-11-15 21:44:02 +0800

    Feat: Extend FoodEffect class to have more funtional

--- src/main/java/com/github/immortalmice/foodpower/effect/FoodEffect.java
+++ src/main/java/com/github/immortalmice/foodpower/effect/FoodEffect.java
@@ -1,12 +1,21 @@
 package com.github.immortalmice.foodpower.effect;
 
+import java.util.UUID;
+
+import net.minecraft.entity.ai.attributes.AttributeModifier.Operation;
+import net.minecraft.entity.ai.attributes.IAttribute;
 import net.minecraft.potion.Effect;
 import net.minecraft.potion.EffectType;
 
-// TODO Is this needed?
 public class FoodEffect extends Effect{
 
 	public FoodEffect(int liquidColorIn){
 		super(EffectType.BENEFICIAL, liquidColorIn);
+	}
+	
+	public FoodEffect(int liquidColorIn, IAttribute attributeIn, int amountIn, Operation operationIn){
+		this(liquidColorIn);
+		
+		this.addAttributesModifier(attributeIn, UUID.randomUUID().toString(), amountIn, operationIn);
 	}
 }
