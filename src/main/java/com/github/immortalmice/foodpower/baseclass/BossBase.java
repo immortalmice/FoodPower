01166a3f05c91bcb63ece9735638faeec2753063
immortalmice <killer536697@gmail.com>
2020-12-12 22:34:16 +0800

    Refactor: BossBase now extends from MonsterEntity

--- src/main/java/com/github/immortalmice/foodpower/baseclass/BossBase.java
+++ src/main/java/com/github/immortalmice/foodpower/baseclass/BossBase.java
@@ -1,16 +1,16 @@
 package com.github.immortalmice.foodpower.baseclass;
 
 import net.minecraft.entity.EntityType;
-import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.monster.MonsterEntity;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.world.BossInfo;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerBossInfo;
 
-public class BossBase extends MobEntity {
+public class BossBase extends MonsterEntity {
 	private final ServerBossInfo bossInfo;
 	
-	public BossBase(EntityType<? extends MobEntity> type, World worldIn, BossInfo.Color bossBarColorIn) {
+	public BossBase(EntityType<? extends MonsterEntity> type, World worldIn, BossInfo.Color bossBarColorIn) {
 		super(type, worldIn);
 		
 		this.bossInfo = new ServerBossInfo(this.getDisplayName(), bossBarColorIn, BossInfo.Overlay.NOTCHED_6);
