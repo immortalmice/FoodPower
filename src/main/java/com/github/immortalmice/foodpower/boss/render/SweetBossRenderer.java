761384157db20744d23496681adc57fe3fd57692
YenHsinWu <kelly850408@gmail.com>
2020-12-13 21:51:45 +0800

    Modify sweet_boss texture and model file

--- src/main/java/com/github/immortalmice/foodpower/boss/render/SweetBossRenderer.java
+++ src/main/java/com/github/immortalmice/foodpower/boss/render/SweetBossRenderer.java
@@ -22,43 +22,43 @@ public class SweetBossRenderer extends MobRenderer<SweetBoss, SweetBossRenderer.
 	}
 
 	public static class SweetBossModel extends EntityModel<SweetBoss> {
-		private final ModelRenderer left_feet;
-		private final ModelRenderer right_feet;
+		private final ModelRenderer left_foot;
+		private final ModelRenderer right_foot;
 		private final ModelRenderer body;
 		private final ModelRenderer left_hand;
 		private final ModelRenderer right_hand;
 		private final ModelRenderer head;
 
 		public SweetBossModel() {
-			textureWidth = 256;
-			textureHeight = 256;
+			textureWidth = 64;
+			textureHeight = 64;
 
-			left_feet = new ModelRenderer(this);
-			left_feet.setRotationPoint(2.0F, 20.0F, 0.0F);
-			left_feet.setTextureOffset(0, 17).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 4.0F, 2.0F, 0.0F, false);
+			left_foot = new ModelRenderer(this);
+			left_foot.setRotationPoint(3.0F, 12.0F, 0.0F);
+			left_foot.setTextureOffset(0, 32).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
-			right_feet = new ModelRenderer(this);
-			right_feet.setRotationPoint(-2.0F, 20.0F, 0.0F);
-			right_feet.setTextureOffset(0, 17).addBox(-1.0F, 0.0F, -1.0F, 2.0F, 4.0F, 2.0F, 0.0F, false);
+			right_foot = new ModelRenderer(this);
+			right_foot.setRotationPoint(-2.0F, 12.0F, 0.0F);
+			right_foot.setTextureOffset(0, 32).addBox(-2.0F, 0.0F, -2.0F, 4.0F, 12.0F, 4.0F, 0.0F, false);
 
 			body = new ModelRenderer(this);
-			body.setRotationPoint(0.0F, 16.0F, 0.0F);
-			body.setTextureOffset(0, 7).addBox(-3.0F, -4.0F, -1.0F, 6.0F, 8.0F, 2.0F, 0.0F, false);
+			body.setRotationPoint(0.5F, 4.5F, 0.0F);
+			body.setTextureOffset(0, 13).addBox(-4.5F, -7.5F, -2.0F, 9.0F, 15.0F, 4.0F, 0.0F, false);
 
 			left_hand = new ModelRenderer(this);
-			left_hand.setRotationPoint(3.0F, 12.0F, 0.0F);
-			left_hand.setTextureOffset(16, 7).addBox(0.0F, 0.0F, -1.0F, 2.0F, 5.0F, 2.0F, 0.0F, false);
+			left_hand.setRotationPoint(5.0F, -3.0F, 0.0F);
+			left_hand.setTextureOffset(26, 13).addBox(0.0F, 0.0F, -2.0F, 4.0F, 10.0F, 4.0F, 0.0F, false);
 
 			right_hand = new ModelRenderer(this);
-			right_hand.setRotationPoint(-3.0F, 12.0F, 0.0F);
-			right_hand.setTextureOffset(16, 7).addBox(-2.0F, 0.0F, -1.0F, 2.0F, 5.0F, 2.0F, 0.0F, false);
+			right_hand.setRotationPoint(-4.0F, -3.0F, 0.0F);
+			right_hand.setTextureOffset(26, 13).addBox(-4.0F, 0.0F, -2.0F, 4.0F, 10.0F, 4.0F, 0.0F, false);
 
 			head = new ModelRenderer(this);
-			head.setRotationPoint(0.0F, 12.0F, 0.0F);
-			head.setTextureOffset(0, 0).addBox(-3.0F, -5.0F, -1.0F, 6.0F, 5.0F, 2.0F, 0.0F, false);
-			head.setTextureOffset(16, 0).addBox(-4.0F, -5.0F, -1.0F, 1.0F, 4.0F, 2.0F, 0.0F, false);
-			head.setTextureOffset(16, 0).addBox(3.0F, -5.0F, -1.0F, 1.0F, 4.0F, 2.0F, 0.0F, false);
-			head.setTextureOffset(22, 0).addBox(-3.0F, -5.0F, 1.0F, 6.0F, 8.0F, 1.0F, 0.0F, false);
+			head.setRotationPoint(0.5F, -3.0F, 0.875F);
+			head.setTextureOffset(0, 0).addBox(-2.5F, -6.0F, -2.875F, 5.0F, 6.0F, 4.0F, 0.0F, false);
+			head.setTextureOffset(18, 0).addBox(-3.5F, -6.0F, -1.875F, 1.0F, 5.0F, 3.0F, 0.0F, false);
+			head.setTextureOffset(18, 0).addBox(2.5F, -6.0F, -1.875F, 1.0F, 5.0F, 3.0F, 0.0F, false);
+			head.setTextureOffset(26, 0).addBox(-2.5F, -6.0F, 1.125F, 5.0F, 12.0F, 1.0F, 0.0F, false);
 		}
 
 		public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
@@ -76,8 +76,8 @@ public class SweetBossRenderer extends MobRenderer<SweetBoss, SweetBossRenderer.
 
 		@Override
 		public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
-			left_feet.render(matrixStack, buffer, packedLight, packedOverlay);
-			right_feet.render(matrixStack, buffer, packedLight, packedOverlay);
+			left_foot.render(matrixStack, buffer, packedLight, packedOverlay);
+			right_foot.render(matrixStack, buffer, packedLight, packedOverlay);
 			body.render(matrixStack, buffer, packedLight, packedOverlay);
 			left_hand.render(matrixStack, buffer, packedLight, packedOverlay);
 			right_hand.render(matrixStack, buffer, packedLight, packedOverlay);
