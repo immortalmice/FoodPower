57b2ab372c1b9057a6680dffba7ca32787cdbe35
immortalmice <killer536697@gmail.com>
2020-12-13 21:53:11 +0800

    Fix: Cream Shield is not damaged on used

--- src/main/java/com/github/immortalmice/foodpower/specialclass/CreamShield.java
+++ src/main/java/com/github/immortalmice/foodpower/specialclass/CreamShield.java
@@ -1,12 +1,20 @@
 package com.github.immortalmice.foodpower.specialclass;
 
 import com.github.immortalmice.foodpower.lists.FPCreativeTabs;
+import com.github.immortalmice.foodpower.lists.OtherItems.Items;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ShieldItem;
 
 public class CreamShield extends ShieldItem {
 	public CreamShield() {
 		super(new Item.Properties().maxDamage(220).group(FPCreativeTabs.ITEM_TAB));
+	}
+
+	@Override
+	public boolean isShield(ItemStack stack, LivingEntity entity) {
+		return super.isShield(stack, entity) || entity.getActiveItemStack().getItem() == Items.CREAM_SHIELD;
 	}
 }
