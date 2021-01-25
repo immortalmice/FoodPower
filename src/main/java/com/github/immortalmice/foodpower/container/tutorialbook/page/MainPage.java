dec03ee1eeb6e403984ab2f6f9828baf087f7ebb
immortalmice <killer536697@gmail.com>
2021-01-25 17:33:24 +0800

    Feat: Basic main page

---
+++ src/main/java/com/github/immortalmice/foodpower/container/tutorialbook/page/MainPage.java
@@ -0,0 +1,7 @@
+package com.github.immortalmice.foodpower.container.tutorialbook.page;
+
+public class MainPage extends AbstractTitlePage {
+	public MainPage() {
+		super("tutorial_book.foodpower.main_page.title");
+	}
+}
