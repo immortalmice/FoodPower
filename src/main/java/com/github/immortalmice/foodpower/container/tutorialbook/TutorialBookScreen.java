a71b1cdf95c6b11a004105af83316425c1033ecc
immortalmice <killer536697@gmail.com>
2021-01-27 12:09:04 +0800

    Refactor: Use Navigator class to control page setter

--- src/main/java/com/github/immortalmice/foodpower/container/tutorialbook/TutorialBookScreen.java
+++ src/main/java/com/github/immortalmice/foodpower/container/tutorialbook/TutorialBookScreen.java
@@ -1,18 +1,12 @@
 package com.github.immortalmice.foodpower.container.tutorialbook;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.github.immortalmice.foodpower.baseclass.ScreenBase;
-import com.github.immortalmice.foodpower.container.tutorialbook.page.IPage;
-import com.github.immortalmice.foodpower.container.tutorialbook.page.MainPage;
 
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.util.text.ITextComponent;
 
 public class TutorialBookScreen extends ScreenBase<TutorialBookContainer> {
 	private int pageIndexCache = -1;
-	private static List<IPage> PAGES = new ArrayList<>();
 	
 	public TutorialBookScreen(TutorialBookContainer containerIn, PlayerInventory inventoryIn, ITextComponent textIn) {
 		super(containerIn, inventoryIn, textIn);
@@ -26,22 +20,18 @@ public class TutorialBookScreen extends ScreenBase<TutorialBookContainer> {
 	protected void drawGuiContainerForegroundLayer(int mouseX, int mouseY) {
 		super.drawGuiContainerForegroundLayer(mouseX, mouseY);
 		
-		if(this.pageIndexCache != this.container.getPage()) {
+		if(this.pageIndexCache != this.container.navigator.getPage()) {
 			this.buttons.clear();
 			this.children.clear();
-			this.pageIndexCache = this.container.getPage();
+			this.pageIndexCache = this.container.navigator.getPage();
 			
-			if(this.pageIndexCache >= 0 && this.pageIndexCache <= PAGES.size() - 1) {
-				TutorialBookScreen.PAGES.get(this.pageIndexCache).init(this::addButton);
+			if(this.pageIndexCache >= 0 && this.pageIndexCache <= TutorialBookContainer.PAGES.size() - 1) {
+				TutorialBookContainer.PAGES.get(this.pageIndexCache).init(this::addButton, this.container.navigator);
 			}
 		}
 		
-		if(this.pageIndexCache >= 0 && this.pageIndexCache <= PAGES.size() - 1) {
-			TutorialBookScreen.PAGES.get(this.pageIndexCache).render(mouseX, mouseY, this, this.font, this.itemRenderer);
+		if(this.pageIndexCache >= 0 && this.pageIndexCache <= TutorialBookContainer.PAGES.size() - 1) {
+			TutorialBookContainer.PAGES.get(this.pageIndexCache).render(mouseX, mouseY, this, this.font, this.itemRenderer);
 		}
-	}
-	
-	static {
-		TutorialBookScreen.PAGES.add(new MainPage());
 	}
 }
