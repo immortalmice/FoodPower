280abc8056b8b01a909f0b368e0a61b849f1ebc5
immortalmice <killer536697@gmail.com>
2021-02-20 22:48:21 +0800

    Feat: SweetBoss AI tasks

--- src/main/java/com/github/immortalmice/foodpower/boss/entities/SweetBoss.java
+++ src/main/java/com/github/immortalmice/foodpower/boss/entities/SweetBoss.java
@@ -3,11 +3,44 @@ package com.github.immortalmice.foodpower.boss.entities;
 import com.github.immortalmice.foodpower.baseclass.BossBase;
 
 import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.SharedMonsterAttributes;
+import net.minecraft.entity.ai.goal.HurtByTargetGoal;
+import net.minecraft.entity.ai.goal.LookAtGoal;
+import net.minecraft.entity.ai.goal.LookRandomlyGoal;
+import net.minecraft.entity.ai.goal.MeleeAttackGoal;
+import net.minecraft.entity.ai.goal.NearestAttackableTargetGoal;
+import net.minecraft.entity.ai.goal.SwimGoal;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.world.BossInfo.Color;
 import net.minecraft.world.World;
 
 public class SweetBoss extends BossBase {
 	public SweetBoss(EntityType<? extends SweetBoss> type, World worldIn) {
 		super(type, worldIn, Color.PINK);
+	}
+	
+	@Override
+	protected void registerAttributes() {
+		super.registerAttributes();
+		
+		this.getAttribute(SharedMonsterAttributes.MAX_HEALTH).setBaseValue(200);
+		this.getAttribute(SharedMonsterAttributes.ATTACK_DAMAGE).setBaseValue(4.0);
+		this.getAttribute(SharedMonsterAttributes.MOVEMENT_SPEED).setBaseValue(4.0);
+		this.getAttribute(LivingEntity.SWIM_SPEED).setBaseValue(4.0);
+		this.getAttribute(SharedMonsterAttributes.FOLLOW_RANGE).setBaseValue(48.0);
+	}
+	
+	@Override
+	protected void registerGoals() {
+		super.registerGoals();
+
+		this.goalSelector.addGoal(2, new MeleeAttackGoal(this, 1.0D, false));
+	    this.goalSelector.addGoal(7, new SwimGoal(this));
+		this.goalSelector.addGoal(8, new LookAtGoal(this, PlayerEntity.class, 8.0F));
+	    this.goalSelector.addGoal(8, new LookRandomlyGoal(this));
+	    
+		this.targetSelector.addGoal(1, new HurtByTargetGoal(this));
+		this.targetSelector.addGoal(2, new NearestAttackableTargetGoal<>(this, PlayerEntity.class, false));
 	}
 }
