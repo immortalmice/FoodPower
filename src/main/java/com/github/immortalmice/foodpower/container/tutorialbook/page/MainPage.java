59130cb842f7b94ddec9872798da6b684807a8fe
immortalmice <killer536697@gmail.com>
2021-03-07 22:26:18 +0800

    Feat: Draw string under buttons

--- src/main/java/com/github/immortalmice/foodpower/container/tutorialbook/page/MainPage.java
+++ src/main/java/com/github/immortalmice/foodpower/container/tutorialbook/page/MainPage.java
@@ -7,11 +7,29 @@ import com.github.immortalmice.foodpower.container.util.IconButton;
 import com.github.immortalmice.foodpower.container.util.IconButton.ButtonType;
 import com.github.immortalmice.foodpower.util.Position2D;
 
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.widget.Widget;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.resources.I18n;
 
 public class MainPage extends AbstractTitlePage {
 	public MainPage() {
 		super("tutorial_book.foodpower.main_page.title");
+	}
+	
+	@Override
+	public void render(int mouseX, int mouseY, Screen screen, FontRenderer font, ItemRenderer itemRenderer) {
+		super.render(mouseX, mouseY, screen, font, itemRenderer);
+		
+		String tutorial = I18n.format("tutorial_book.foodpower.main_page.tutorial");
+		font.drawString(tutorial, (75 - font.getStringWidth(tutorial) / 2), 128, 0x404040);
+		String ingredient = I18n.format("tutorial_book.foodpower.main_page.ingredient");
+		font.drawString(ingredient, (184 - font.getStringWidth(ingredient) / 2), 128, 0x404040);
+		String meal = I18n.format("tutorial_book.foodpower.main_page.meal");
+		font.drawString(meal, (75 - font.getStringWidth(meal) / 2), 220, 0x404040);
+		String boss = I18n.format("tutorial_book.foodpower.main_page.boss");
+		font.drawString(boss, (184 - font.getStringWidth(boss) / 2), 220, 0x404040);
 	}
 
 	@Override
